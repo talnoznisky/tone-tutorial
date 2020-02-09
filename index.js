c75c1be3ad1bf5064534bd14d3d0df39f4c0c21f
talnoznisky <tal.noznisky@gmail.com>
2020-02-09 12:41:07 -0600

    tail whip animation working

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import ConfettiGenerator from '.node-modules/confetti-js/dist/index.js';
+loadCircles = () => {
+  let offset = 0
+  for(var i = 0;i < 12; i++){
+    const circle = document.createElement('div')
+    circle.setAttribute('class', 'circle up');
+    circle.style.animationDelay = offset.toString() + 's'
+    container.append(circle)
+    offset += .2
+  }
+}
 
-var confettiSettings = { target: 'my-canvas' };
-var confetti = new ConfettiGenerator(confettiSettings);
-confetti.render();
-
-console.log('hmmm')
+loadCircles()
